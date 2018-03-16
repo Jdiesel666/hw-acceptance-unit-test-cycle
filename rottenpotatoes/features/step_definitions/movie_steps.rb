2aab86adca7c5211fdaa26cc54753f922b477db9
Han Bee Oh <ohb_abr@tamu.edu>
2018-03-16 02:47:22 +0000

    scenario 1 done

---
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -0,0 +1,10 @@
+Given(/^the following movies exist:$/) do |movies_table|
+    movies_table.hashes.each do |movie|
+        Movie.create!(movie)
+    end
+    expect(Movie.all.count).to eq 4
+end
+
+Then(/^the director of "(.*)" should be "(.*)"/) do |title, director|
+    expect(Movie.find_by_title(title).director).to eq director
+end
